e4d73a15d2390474eebad118dc1132c25a7310a4
wangjin <wangjin@maijinbei.com>
2018-05-31 15:39:31 +0800

    Git 使用方式 1.0

---
+++ app/src/main/java/com/ww/mygit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ww.mygit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Git使用方式 1.0
+    }
+}
