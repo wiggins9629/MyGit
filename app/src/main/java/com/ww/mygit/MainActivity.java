f675004dbed43837d369f0189ad32d43ef4a2721
wangjin <wangjin@maijinbei.com>
2018-06-11 09:19:40 +0800

    修改Git使用方式1.0.0

--- app/src/main/java/com/ww/mygit/MainActivity.java
+++ app/src/main/java/com/ww/mygit/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Git使用方式 1.0
+        // Git使用方式 1.0.0
     }
 }
